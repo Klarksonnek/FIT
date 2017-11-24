3eed7cd70637b9f45eaa9e44c611cff99886252e
Klara <klarksonn@gmail.com>
2017-11-24 09:56:45 +0100

    BMS1: BTSData - add comments
    Signed-off-by: Klara <klarksonn@gmail.com>

--- BMS_1/BTSData.cpp
+++ BMS_1/BTSData.cpp
@@ -9,41 +9,73 @@ BTSData::BTSData(int CID):
 {
 }
 
+/**
+ * Sets GPS coordinates.
+ * @param GPS    GPS coordinates.
+ */
 void BTSData::setGPSCoordinate(Coordinates::Ptr GPS)
 {
 	m_GPSCoordinate = GPS;
 }
 
+/**
+ * Returns GPS coordinates.
+ * @return    GPS coordinates.
+ */
 Coordinates::Ptr BTSData::GPSCoordinate() const
 {
 	return m_GPSCoordinate;
 }
 
+/**
+ * Sets signal measured on mobile station.
+ * @param signal    Signal measured on mobile station.
+ */
 void BTSData::setSignal(double signal)
 {
 	m_signal = signal;
 }
 
+/**
+ * Returns signal measured on mobile station.
+ * @return    Signal measured on mobile station.
+ */
 double BTSData::signal() const
 {
 	return m_signal;
 }
 
+/**
+ * Sets height of BTS antenna.
+ * @param height    Heigth of BTS antenna.
+ */
 void BTSData::setHeight(double height)
 {
 	m_BTSHeight = height;
 }
 
+/**
+ * Returns heigth of BTS antenna.
+ * @return    Heigth of BTS antenna.
+ */
 double BTSData::height() const
 {
 	return m_BTSHeight;
 }
 
+/**
+ * Sets BTS power.
+ * @param power    Power.
+ */
 void BTSData::setPower(double power)
 {
 	m_power = power;
 }
 
+/**
+ * Return BTS power.
+ * @return    Power.
+ */
 double BTSData::power() const
 {
 	return m_power;
