5ccca3cdbb63b8619134779d3e0fa223f74eea00
Klara <klarksonn@gmail.com>
2017-11-24 19:31:57 +0100

    BMS1: main - edit comments and rename variables, methods
    Signed-off-by: Klara <klarksonn@gmail.com>

--- BMS_1/main.cpp
+++ BMS_1/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <map>
 
+#include "BTSData.h"
 #include "BTSLoadData.h"
 #include "CustomException.h"
-#include "BTSData.h"
 #include "Distance.h"
 
 const int FREQUENCY = 900;
@@ -12,22 +12,28 @@ const double HEIGHT = 1.2;
 using namespace std;
 
 /**
- * Initializes list of BTS stations.
- * @param m_BTSFile    File name containing list of BTS.
- * @param m_BTSData    Information about BTS.
- * @return    True if initialization is successful, false otherwise.
+ * Initializes list of BTS.
+ * @param   BTSFile     File name containing list of BTS.
+ * @param   BTSInfo     Information about BTS.
+ * @return  True if initialization is successful, false otherwise.
  */
-bool init(const string &m_BTSFile, map<BTSData::BTSKey, BTSData::Ptr> &m_BTSData)
+bool init(const string &BTSFile, map<BTSData::BTSKey, BTSData::Ptr> &BTSInfo)
 {
 	BTSLoadData data;
 
-	if (!data.open(m_BTSFile, ';')) {
-		cerr << "file " + m_BTSFile + " with list of BTS cannot be loaded" << endl;
+	if (!data.open(BTSFile, ';')) {
+		cerr << "file " + BTSFile + " with list of BTS cannot be loaded" << endl;
 		return false;
 	}
 
+    if (BTSFile.empty()) {
+        cerr << "no list of BTS in file" << endl;
+        return false;
+    }
+
 	data.rawRow();
 
+	// process list of BTS and store CID and GPS coordinates
 	while (!data.isEOF()) {
 		vector<string> row = data.rawRow();
 
@@ -36,12 +42,13 @@ bool init(const string &m_BTSFile, map<BTSData::BTSKey, BTSData::Ptr> &m_BTSData
 
 		int CID = stoi(row[0], nullptr, 10);
 
-		BTSData::Ptr bts = new BTSData(CID);
-		bts->setGPSCoordinate(Coordinates::DMSToDD(row[4]));
+		// save GPS coordinates of BTS
+		BTSData::Ptr BTS = new BTSData(CID);
+		BTS->setGPSCoordinate(Coordinates::DegMinSecToDecDeg(row[4]));
 
 		BTSData::BTSKey btsKey;
 		btsKey = CID;
-		m_BTSData.emplace(btsKey, bts);
+		BTSInfo.emplace(btsKey, BTS);
 	}
 
 	return true;
@@ -49,19 +56,24 @@ bool init(const string &m_BTSFile, map<BTSData::BTSKey, BTSData::Ptr> &m_BTSData
 
 /**
  * Initializes BTS from input file.
- * @param m_nearBTSFIle    File name containing list of near BTS.
- * @param m_BTSData    Information about BTS.
- * @param m_nearBTS    Information about near BTS.
- * @return   True if initialization is successful, false otherwise.
+ * @param   nearBTSFile     File name containing list of near BTS.
+ * @param   BTSInfo         Information about BTS.
+ * @param   nearBTSInfo     Information about near BTS.
+ * @return  True if initialization is successful, false otherwise.
  */
-bool initNearBTS(const string &m_nearBTSFIle, map<BTSData::BTSKey, BTSData::Ptr> &m_BTSData, vector<BTSData::Ptr> &m_nearBTS)
+bool initNearBTS(const string &nearBTSFile, map<BTSData::BTSKey, BTSData::Ptr> &BTSInfo, vector<BTSData::Ptr> &nearBTSInfo)
 {
 	BTSLoadData data;
 
-	if (!data.open(m_nearBTSFIle, ';')) {
-		cerr << "file " + m_nearBTSFIle + " with list of BTS cannot be loaded" << endl;
+	if (!data.open(nearBTSFile, ';')) {
+		cerr << "file " + nearBTSFile + " with list of BTS cannot be loaded" << endl;
 		return false;
 	}
+
+    if (data.isEOF()) {
+        cerr << "no information about near BTS in input file" << endl;
+        return false;
+    }
 
 	data.rawRow();
 
@@ -74,13 +86,14 @@ bool initNearBTS(const string &m_nearBTSFIle, map<BTSData::BTSKey, BTSData::Ptr>
 		int CID = stoi(row[1], nullptr, 10);
 
 		bool found = false;
-		for (auto &it : m_BTSData) {
+		// store more detailed information about near BTS
+		for (auto &it : BTSInfo) {
 			if (it.first == CID) {
 				it.second->setSignal(stoi(row[3], nullptr, 10));
 				it.second->setHeight(stof(row[4], nullptr));
 				it.second->setPower(stoi(row[5], nullptr, 10));
 
-				m_nearBTS.push_back(it.second);
+				nearBTSInfo.push_back(it.second);
 				found = true;
 
 				break;
@@ -97,24 +110,24 @@ bool initNearBTS(const string &m_nearBTSFIle, map<BTSData::BTSKey, BTSData::Ptr>
 }
 
 /**
- * Counts distance between base and mobile stations.
- * @param m_nearBTS    Information about near BTS.
- * @param m_distance    Distance between base and mobile stations.
+ * Computes distance between base and mobile stations using Hata model.
+ * @param nearBTSInfo    Information about near BTS.
+ * @param distance       Distance between base and mobile stations.
  */
-void countDistance(vector<BTSData::Ptr> &m_nearBTS, vector<Distance::BTSDistance> &m_distance)
+void computeDistance(vector<BTSData::Ptr> &nearBTSInfo, vector<Distance::BTSDistance> &distance)
 {
-	for (auto BTS : m_nearBTS) {
-		double Lu = Distance::countPathLoss(BTS->power(), BTS->signal());
-		double distance = Distance::countDistance(
-			Lu, FREQUENCY, BTS->height(),
-			Distance::smallOrMediumSizedCity(FREQUENCY, HEIGHT));
-		m_distance.push_back({BTS, distance});
+	for (auto BTS : nearBTSInfo) {
+		double Lu = Distance::computePathLoss(BTS->power(), BTS->signal());
+		double dist = Distance::computeDistance(
+                Lu, FREQUENCY, BTS->height(),
+                Distance::smallOrMediumSizedCity(FREQUENCY, HEIGHT));
+		distance.push_back({BTS, dist});
 	}
 }
 
 /**
- * Creates and saves file containing link to Google Maps with reference * to the calculated coordinates.
- * @param GPS
+ * Creates and saves file containing link to Google Maps with reference to the calculated coordinates.
+ * @param GPS      GPS coordinates.
  */
 void save(Coordinates::Ptr GPS)
 {
@@ -126,13 +139,9 @@ void save(Coordinates::Ptr GPS)
 		output.close();
 	}
 	else {
-		cerr << "GPS coordinates of mobile station was not save" << endl;
+		cerr << "GPS coordinates of mobile station were not saved" << endl;
 	}
 }
-
-// TODO - otestovat ci vsetko funguje ako ma
-// TODO - premenovat niektore premenne a niektore premenne v niektorych triedach
-// TODO - upravit Makefile pre testy
 
 /**
  * Main program.
@@ -143,24 +152,24 @@ int main(int argc, char* argv[])
 		cerr <<  "file containing information about BTS was not entered" << endl;
 	}
 
-	map<BTSData::BTSKey, BTSData::Ptr> m_BTSData;
-	vector<BTSData::Ptr> m_nearBTS;
-	vector<Distance::BTSDistance> m_distance;
+	map<BTSData::BTSKey, BTSData::Ptr> BTSInfo;
+	vector<BTSData::Ptr> nearBTSInfo;
+	vector<Distance::BTSDistance> distance;
 
 	try {
-		if (!init("bts.csv", m_BTSData) || !initNearBTS(argv[1], m_BTSData, m_nearBTS))
+		// load input file and file containing BTS
+		if (!init("bts.csv", BTSInfo) || !initNearBTS(argv[1], BTSInfo, nearBTSInfo))
 			return EXIT_FAILURE;
 
-		//TODO: check if input file is not empty
-		countDistance(m_nearBTS, m_distance);
+		// compute distances between mobile and base stations
+		computeDistance(nearBTSInfo, distance);
 
+		// find GPS coordinates of mobile station
 		Distance location;
+		Coordinates::Ptr GPS = location.findMS(distance);
 
-		Coordinates::Ptr GPS = location.findMS(m_distance);
-
-		// TODO: check if GPS coordinates are valid
+		// save link to Google Maps
 		save(GPS);
-
 	}
 	catch (const CustomException &ex) {
 		cerr << ex.message() << endl;
