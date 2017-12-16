73b9b586fdda029f57951eba7f3fa9993a043723
Klara <klarksonn@gmail.com>
2017-12-16 11:26:21 +0100

    BMS2: edit it_is_ok.sh
    Signed-off-by: Klara <klarksonn@gmail.com>

--- BMS_2/it_is_ok.sh
+++ BMS_2/it_is_ok.sh
@@ -1,89 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-BMS2A="./bms2A"
+LOGIN=$1
 
-INPUT_DIR="input"
-REF_OUTPUT="ref_output"
+if [ -z ${LOGIN} ]; then
+	echo "Musi byt zadany login"
+	exit 1
+fi
 
-ERROR=0
-ERR_CODE=0
-SEPARATOR="-------------------------------------------"
+/bin/rm -rf ${LOGIN}
+unzip ${LOGIN} -d ${LOGIN}
 
-echoTest() {
-	echo -n -e "\e[36m"
-	echo "$1"
-	echo -n -e "\e[39m"
-}
+cp run_tests.sh ${LOGIN}/.
+cp -r dir_wav ${LOGIN}/.
+cp -r dir_txt ${LOGIN}/.
+cp libsndfile.a ${LOGIN}/.
 
-check () {
-	ERR=$?
-	ERR_CODE=$ERR
-	OK=0
+cd ${LOGIN}
+make -j`nproc`
+#pushd ${LGIN}
+./run_tests.sh
+#popd
 
-	#zistenie ci je vysledok programu Ok/ERR
-	if [ $ERR -eq 0 ] ; then
-		OK=1
-	fi
 
-	#zelenou sa potvrdi spravny exit code
-	#cercenou je chybny navratovy kod
-	if [ "$1" == "OK"  ] && [  $OK -eq 1 ] ; then
-		echo -ne "\e[92m"
-	elif  [ "$1" == "ERR"  ] && [  $OK -eq 0 ] ; then
-		echo -ne "\e[92m"
-	else
-		echo -ne "\e[31m"
-		ERROR=$((ERROR + 1)) #pocet chyb
-	fi
-
-	#vypis vysledku
-	if [ $ERR -eq 0 ] ; then
-		echo "OK"
-	else
-		echo "ERR: $ERR"
-	fi
-
-	#reset farby
-	echo -n -e "\e[39m"
-}
-
-test() {
-    echoTest "${1}"
-    $BMS2A "${2}"
-    check "${3}"
-    echo "$SEPARATOR"
-}
-
-testWAW() {
-    echoTest "${1}"
-    $BMS2A "${INPUT_DIR}/${2}"
-    check "${3}"
-    diff "${INPUT_DIR}/${2}.waw" "${REF_OUTPUT}/${2}.waw"
-    check "OK"
-    echo "$SEPARATOR"
-}
-
-echo -ne "\e[31m"
-echo "Testy do predmetu BMS - projekt 2"
-echo "Testy su urcene pre nasledujucu modulaciu s 15 vzorkami na udaj: "
-echo -e "\t00 - 3pi/4"
-echo -e "\t01 - 1pi/4"
-echo -e "\t10 - 5pi/4"
-echo -e "\t11 - 7pi/4"
-echo ""
-echo "Vstup modulacie sa nachadza v adresari: '${INPUT_DIR}'"
-echo "Referencne vystupy sa nachadzaju v adresari: '${REF_OUTPUT}'"
-echo "Vygenerovany vystup sa nachadza v adresari: '${INPUT_DIR}'"
-echo -e "\n\n"
-echo -n -e "\e[39m"
-
-################################################
-test "00 - chybajuci argument" "" "ERR" 1
-test "01 - privela argumentov" "jeden dva tri" "ERR" 1
-test "02 - neexistujuci subor ako argument" "" "ERR" 1
-test "03 - nevalidny vstupny subor - subor obsahuje okrem 0,1 aj ine znaky" "Makefile" "ERR" 1
-
-################################################
-
-testWAW "10 - zakladna modulacia" "test10.txt" "OK" 0
 
