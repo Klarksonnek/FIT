2503fcf2163e1b3af740b50952357fd66c14e92f
Klara Necasova <xnecas24@stud.fit.vutbr.cz>
2019-04-28 10:15:37 +0200

    kry: proj2 - testing
    Signed-off-by: Klara Necasova <xnecas24@stud.fit.vutbr.cz>

--- KRY_2/ecc.py
+++ KRY_2/ecc.py
@@ -2,11 +2,11 @@
 
 import argparse
 
-F_P = 0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff
-A = -0x3
-B = 0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b
-P_X = 0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296
-P_Y = 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5
+F_P = 0x17
+A = 0x01
+B = 0x01
+P_X = 0xD
+P_Y = 0x10
 
 
 # get inverse number
